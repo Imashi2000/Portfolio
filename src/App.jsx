d16bfc5e5763054a6bb74a00638a8bfa618182e5
Imashi Samarawickarama <INT.Imashi@CreativeSoftware.com>
2024-10-06 00:32:21 +0530

    created contact me section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Profile from './components/ProfileSection/Profile';
 import Skills from './components/Skills/Skills';
 import { WorkExperience } from './components/WorkExperience/WorkExperience';
 import Projects from './components/Projects/Projects';
+import ContactMe from './components/ContactMe/ContactMe';
 
 const App = () =>
 {
@@ -16,6 +17,7 @@ const App = () =>
         <Skills />
         <WorkExperience />
         <Projects />
+        <ContactMe />
       </div>
     </>
   )
