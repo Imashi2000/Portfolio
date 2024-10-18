7524eeef73ab2097220bcc24c907ec15d53b68d3
Imashi Samarawickarama <INT.Imashi@CreativeSoftware.com>
2024-10-18 14:33:53 +0530

    Created the hire me sction

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,19 +2,33 @@ import React from 'react'
 import './Navbar.css'
 import MobileNav from './MobileNav/MobileNav';
 import { useState } from 'react';
+import HireMe from '../HireMe/HireMe';
 
 const Navbar = () =>
 {
     const [openMenu, setOpenMenu] = useState(false);
+    const [isHireMeOpen, setIsHireMeOpen] = useState(false);
 
     const toggleMenu = () =>
     {
         setOpenMenu(!openMenu);
+        if (isHireMeOpen) {
+            setIsHireMeOpen(false); // Close modal if it’s open
+        }
+    };
+
+    const toggleModal = () =>
+    {
+        setIsHireMeOpen(!isHireMeOpen);
     };
 
     return (
         <>
-            <MobileNav isOpen={openMenu} toggleMenu={toggleMenu} />
+            <MobileNav 
+                isOpen={openMenu} 
+                toggleMenu={toggleMenu} 
+                toggleModal={toggleModal} 
+            />
 
             <nav className='nav-wrapper'>
                 <div className='nav-content'>
@@ -39,7 +53,7 @@ const Navbar = () =>
                             <a className='menu-item' href="#CONTACT">Contact Me</a>
                         </li>
 
-                        <button className='contact-btn' onClick={() => { }}>
+                        <button className="contact-btn" onClick={toggleModal}>
                             Hire Me
                         </button>
                     </ul>
@@ -55,6 +69,7 @@ const Navbar = () =>
                     </button>
                 </div>
             </nav>
+            {isHireMeOpen && <HireMe onClose={toggleModal} />}
         </>
     )
 }
